c0c957a21b50d94d2c1cbc1a2e91b99fe5e6ce6f
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-19 12:46:52 +0200

    Create Intermediate-Task6.sql

---
+++ Intermediate/Intermediate-Task6.sql
@@ -0,0 +1,10 @@
+/*
+Show 10 orders with the most line items, in order of total line items.
+As a hint Using Orders and OrderDetails, you'll use Group by and count() functionality.
+*/
+
+SELECT TOP 10 Orders.OrderID, COUNT(OrderDetails.OrderID) AS NumberOfLineItems
+FROM Orders
+JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+GROUP BY Orders.OrderID
+ORDER BY NumberOfLineItems DESC

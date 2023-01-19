2eaf0245efc0840f01a4d157237908a45a3615e6
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-19 11:52:46 +0200

    Update Task4.sql

--- Basic Tasks/Task4.sql
+++ Basic Tasks/Task4.sql
@@ -1,3 +1,7 @@
+/*
+Find quantity of employees in each department. Departments is the same as a title in the Employees table.
+*/
+
 SELECT Title AS Department, COUNT(EmployeeID) AS 'qty of employees'
-FROM [Northwind].[dbo].[Employees]
+FROM Northwind.dbo.Employees
 GROUP BY Title

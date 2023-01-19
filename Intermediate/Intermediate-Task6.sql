6a50062059ddb2a1734e2387e28255a441d58e41
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-19 12:58:39 +0200

    Update Intermediate-Task6.sql

--- Intermediate/Intermediate-Task6.sql
+++ Intermediate/Intermediate-Task6.sql
@@ -4,7 +4,7 @@ As a hint Using Orders and OrderDetails, you'll use Group by and count() functio
 */
 
 SELECT TOP 10 Orders.OrderID, COUNT(OrderDetails.OrderID) AS NumberOfLineItems
-FROM Orders
-JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+FROM Northwind.dbo.Orders
+JOIN Northwind.dbo.OrderDetails ON Orders.OrderID = OrderDetails.OrderID
 GROUP BY Orders.OrderID
 ORDER BY NumberOfLineItems DESC

cb12dfa00e83002776ded2f1450a11fd40d607a6
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-19 12:55:20 +0200

    Create Intermediate-Task7.sql

---
+++ Intermediate/Intermediate-Task7.sql
@@ -0,0 +1,30 @@
+/*
+Get the number of products that were delivered by each supplier (company), which have a discount from the Unit Price more than 20%.
+Order records are represented from the biggest to lowest discount.
+*/
+
+/*
+The first SELECT statement is just for displaying available data that I am working with.
+*/
+
+SELECT Suppliers.SupplierID, CompanyName AS 'CompanyName(Suppliers)', OrderID AS 'OrderID(OrderDetails)',
+ProductName AS 'ProductName(Products)', Quantity AS 'Quantity(OrderDetails)',
+OrderDetails.UnitPrice AS 'UnitPrice(OrderDetails)', Products.UnitPrice AS 'UnitPrice(Products)', Discount AS 'Discount(OrderDetails)'
+FROM Northwind.dbo.Suppliers
+JOIN Northwind.dbo.Products ON Suppliers.SupplierID = Products.SupplierID
+JOIN Northwind.dbo.OrderDetails ON Products.ProductID = OrderDetails.ProductID
+WHERE Discount >= 0.2
+ORDER BY SupplierID
+
+/*
+The second SELECT statement shows the number of delivered products (with certain discount) for each supplier.
+But it does not sort records by discount.
+*/
+
+SELECT CompanyName, SUM(Quantity) AS TotalNumber
+FROM Northwind.dbo.Suppliers
+JOIN Northwind.dbo.Products ON Suppliers.SupplierID = Products.SupplierID
+JOIN Northwind.dbo.OrderDetails ON Products.ProductID = OrderDetails.ProductID
+WHERE Discount > 0.2
+GROUP BY Suppliers.CompanyName
+ORDER BY TotalNumber DESC

0eea34dc5c3aa524f21ed251705ba6d860e3f963
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-19 12:44:25 +0200

    Create Intermediate-Task5.sql

---
+++ Intermediate/Intermediate-Task5.sql
@@ -0,0 +1,11 @@
+/*
+Select EmployeeID, LastName, OrderID, ProductName and Quantity and sort by OrderID and Product ID.
+As a hint you'll need to do a join between 4 tables, displaying only those fields that are necessary.
+*/
+
+SELECT Employees.EmployeeID, LastName, Orders.OrderID, ProductName, Quantity
+FROM Employees
+JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
+JOIN Products ON OrderDetails.ProductID = Products.ProductID
+ORDER BY OrderID, Products.ProductID

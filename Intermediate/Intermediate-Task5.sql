f1954ee5c91036888d86ee956c2da1d904a0bf02
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-23 15:29:51 +0200

    Update Intermediate-Task5.sql

--- Intermediate/Intermediate-Task5.sql
+++ Intermediate/Intermediate-Task5.sql
@@ -3,9 +3,10 @@ Select EmployeeID, LastName, OrderID, ProductName and Quantity and sort by Order
 As a hint you'll need to do a join between 4 tables, displaying only those fields that are necessary.
 */
 
+USE Northwind
 SELECT Employees.EmployeeID, LastName, Orders.OrderID, ProductName, Quantity
-FROM Northwind.dbo.Employees
-JOIN Northwind.dbo.Orders ON Employees.EmployeeID = Orders.EmployeeID
-JOIN Northwind.dbo.OrderDetails ON OrderDetails.OrderID = Orders.OrderID
-JOIN Northwind.dbo.Products ON OrderDetails.ProductID = Products.ProductID
+FROM Employees
+JOIN Orders ON Employees.EmployeeID = Orders.EmployeeID
+JOIN OrderDetails ON OrderDetails.OrderID = Orders.OrderID
+JOIN Products ON OrderDetails.ProductID = Products.ProductID
 ORDER BY OrderID, Products.ProductID

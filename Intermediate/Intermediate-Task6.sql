386a1bac732fad5465261934883dfaebdea9c80d
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-23 15:46:19 +0200

    Update Intermediate-Task6.sql

--- Intermediate/Intermediate-Task6.sql
+++ Intermediate/Intermediate-Task6.sql
@@ -3,8 +3,9 @@ Show 10 orders with the most line items, in order of total line items.
 As a hint Using Orders and OrderDetails, you'll use Group by and count() functionality.
 */
 
+USE Northwind
 SELECT TOP 10 Orders.OrderID, COUNT(OrderDetails.OrderID) AS NumberOfLineItems
-FROM Northwind.dbo.Orders
-JOIN Northwind.dbo.OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+FROM Orders
+JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
 GROUP BY Orders.OrderID
 ORDER BY NumberOfLineItems DESC

6b44ca1eb2d86fd3a3aff86ec4308bb077f9f801
argentumkun <98430175+argentumkun@users.noreply.github.com>
2023-01-30 00:31:40 +0200

    Update Intermediate-Task4.sql

--- Intermediate/Intermediate-Task4.sql
+++ Intermediate/Intermediate-Task4.sql
@@ -2,8 +2,21 @@
 Find the list of the same Postal Codes between Suppliers and Customers tables.
 */
 
+-- 1st variant - with JOIN. Not the best choice here.
+
+/*
 USE Northwind
 SELECT SupplierID, CustomerID, Suppliers.PostalCode
 FROM Suppliers
 INNER JOIN Customers
 ON Suppliers.PostalCode = Customers.PostalCode
+*/
+
+-- 2nd variant - with INTERSECT. Removes duplicates. Shows nulls.
+
+USE Northwind
+SELECT PostalCode
+FROM Suppliers
+INTERSECT
+SELECT PostalCode
+FROM Customers
